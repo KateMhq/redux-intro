00ec564c3b89b1d5b418f2571b494cdf38bc096e
Kate <mhq0612@163.com>
2018-10-30 11:19:25 +0000

    exe done

--- src/reducers/countriesVisited.js
+++ src/reducers/countriesVisited.js
@@ -1,12 +1,18 @@
-function countriesVisited(state = {
+const initialState={
   countriesVisited: []
-}, action){
+}; //state might get complicated
+
+
+function countriesVisited(state = initialState, action){
   switch(action.type){
     case 'ADD_COUNTRY':
+      const newCountriesArr = state.countriesVisited.concat([action.country]);
+      //alternatively we can use spread
+      //const newCountriesArr=[...state.countriesVisited, action.country]
       const newCountryAdded = Object.assign({}, state, {
-      countriesVisited: state.countriesVisited.concat([action.country])
+      countriesVisited: newCountriesArr
     });
-      return newCountryAdded;
+      return newCountryAdded; //return a clone of state
 
     case 'DELETE_COUNTRY':
       const updatedArr = state.countriesVisited.filter(country=> country !== action.country)
